0910bd7b00672e2e97f731fe99db2a821cd35d5a
rutgercolon <colonrutger@gmail.com>
2022-09-23 15:59:04 -0400

    Delete repository that wasn't being used

--- app/src/main/java/com/example/rutgersweatherapp/repository/WeatherRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.example.rutgersweatherapp.repository
-
-import com.example.rutgersweatherapp.api.WeatherApi
-
-class WeatherRepository(private val instance: WeatherApi) {
-
-    suspend fun getCurrentWeather(key: String, query: String, days: Int) = instance.getForecastWeather(key, query, days)
-}
