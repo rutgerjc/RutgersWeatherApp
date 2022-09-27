738d0b423dc84e31fe589214732dea17af7cc960
rutgercolon <colonrutger@gmail.com>
2022-09-27 18:45:55 -0400

    Updated editText to response with an empty field with out a crash

--- app/src/main/java/com/example/rutgersweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/rutgersweatherapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.rutgersweatherapp
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Looper
 import android.view.KeyEvent.KEYCODE_ENTER
 import android.view.View
 import android.view.inputmethod.EditorInfo
@@ -10,10 +11,12 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import com.example.rutgersweatherapp.api.RetrofitInstance
 import com.example.rutgersweatherapp.api.WeatherApi
 import com.squareup.picasso.Picasso
 import kotlinx.coroutines.*
+import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
@@ -60,15 +63,20 @@ class MainActivity : AppCompatActivity() {
             if (cityName.isNotEmpty()) {
                 getWeather(cityName)
                 closeKeyBoard(searchIV)
+            } else{
+                Toast.makeText(this, "Input a city name", Toast.LENGTH_SHORT).show()
+                closeKeyBoard(searchIV)
             }
         }
         cityNameET.setOnEditorActionListener { v, actionId, event ->
-            if (actionId == EditorInfo.IME_ACTION_DONE || event.keyCode == KEYCODE_ENTER) {
-                cityName = cityNameET.text.toString()
+            cityName = cityNameET.text.toString()
+            if (actionId == EditorInfo.IME_ACTION_DONE || event.keyCode == KEYCODE_ENTER && cityName.isNotEmpty()) {
                 getWeather(cityName)
                 closeKeyBoard(cityNameET)
                 true
             } else {
+                Toast.makeText(this, "Input a city name", Toast.LENGTH_SHORT).show()
+                closeKeyBoard(cityNameET)
                 false
             }
         }
@@ -77,27 +85,36 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
     @OptIn(DelicateCoroutinesApi::class)
     private fun getWeather(cityName: String){
-        val getWeatherInfo = RetrofitInstance.getWeather(cityName).create(WeatherApi::class.java)
-        GlobalScope.launch {
-            val result = getWeatherInfo.getForecastWeather(BuildConfig.WEATHER_API_KEY, cityName, 7)
-            val data = result.body()!!
+            val getWeatherInfo = RetrofitInstance.getWeather(cityName).create(WeatherApi::class.java)
+            GlobalScope.launch {
 
-            withContext(Dispatchers.Main){
-                locationTV.text = data.location.name
-                dateTV.text = data.location.localtime
-                Picasso.get().load("http:" + data.current.condition.icon).into(currentWeatherIV)
-                currentTempTV.text = data.current.temp_f.toString() + "° F"
-                maxTempTV.text = data.forecast.forecastday[0].day.maxtemp_f.toString() + "° F"
-                minTempTV.text = data.forecast.forecastday[0].day.mintemp_f.toString() + "° F"
-                sunRiseTV.text = data.forecast.forecastday[0].astro.sunrise
-                sundownTV.text = data.forecast.forecastday[0].astro.sunset
-                windSpeedTV.text = data.current.wind_mph.toString() + "mph"
-                cloudyTV.text = data.current.cloud.toString() + "%"
-                rainyTV.text =
-                    data.forecast.forecastday[0].day.daily_chance_of_rain.toString() + "%"
-                humidityTV.text = data.forecast.forecastday[0].day.avghumidity.toString() + "%"
+                val result =
+                    getWeatherInfo.getForecastWeather(BuildConfig.WEATHER_API_KEY, cityName, 7)
+                if (result.isSuccessful) {
+                    val data = result.body()!!
+                    withContext(Dispatchers.Main) {
+                        locationTV.text = data.location.name
+                        dateTV.text = data.location.localtime
+                        Picasso.get().load("http:" + data.current.condition.icon)
+                            .into(currentWeatherIV)
+                        currentTempTV.text = data.current.temp_f.toString() + "° F"
+                        maxTempTV.text =
+                            data.forecast.forecastday[0].day.maxtemp_f.toString() + "° F"
+                        minTempTV.text =
+                            data.forecast.forecastday[0].day.mintemp_f.toString() + "° F"
+                        sunRiseTV.text = data.forecast.forecastday[0].astro.sunrise
+                        sundownTV.text = data.forecast.forecastday[0].astro.sunset
+                        windSpeedTV.text = data.current.wind_mph.toString() + "mph"
+                        cloudyTV.text = data.current.cloud.toString() + "%"
+                        rainyTV.text =
+                            data.forecast.forecastday[0].day.daily_chance_of_rain.toString() + "%"
+                        humidityTV.text =
+                            data.forecast.forecastday[0].day.avghumidity.toString() + "%"
+                    }
+                } else {
+                    return@launch
+                }
             }
-        }
     }
 
     private fun closeKeyBoard(view: View) {
