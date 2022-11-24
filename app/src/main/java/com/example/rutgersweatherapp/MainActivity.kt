1c35ba8659421ac8f32337a81d3dbcdf8211028f
rutgerjc <colonrutger@gmail.com>
2022-11-23 22:37:40 -0500

    Fixed issue that was causing the app to crash, invoking suspend functions correctly

--- app/src/main/java/com/example/rutgersweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/rutgersweatherapp/MainActivity.kt
@@ -1,119 +1,84 @@
 package com.example.rutgersweatherapp
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.KeyEvent.KEYCODE_ENTER
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
-import android.widget.EditText
-import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
 import com.example.rutgersweatherapp.BuildConfig.WEATHER_API_KEY
 import com.example.rutgersweatherapp.api.RetrofitInstance
-import com.example.rutgersweatherapp.api.WeatherApi
-import com.squareup.picasso.BuildConfig
+import com.example.rutgersweatherapp.databinding.ActivityMainBinding
 import com.squareup.picasso.Picasso
 import kotlinx.coroutines.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var cityNameET: EditText
-    lateinit var cityName: String
-    lateinit var searchIV: ImageView
-    lateinit var locationTV: TextView
-    lateinit var dateTV: TextView
-    lateinit var currentWeatherIV: ImageView
-    lateinit var currentTempTV: TextView
-    lateinit var minTempTV: TextView
-    lateinit var maxTempTV: TextView
-    lateinit var sunRiseTV: TextView
-    lateinit var sundownTV: TextView
-    lateinit var windSpeedTV: TextView
-    lateinit var humidityTV: TextView
-    lateinit var cloudyTV: TextView
-    lateinit var rainyTV: TextView
+    private lateinit var binding: ActivityMainBinding
 
+    private lateinit var cityName: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        cityNameET = findViewById(R.id.ETCityName)
-        searchIV = findViewById(R.id.IVSearch)
-        locationTV = findViewById(R.id.TVLocation)
-        dateTV = findViewById(R.id.TVDate)
-        currentWeatherIV = findViewById(R.id.IVWeatherCondition)
-        currentTempTV = findViewById(R.id.TVTemperature)
-        minTempTV = findViewById(R.id.TVMinTemp)
-        maxTempTV = findViewById(R.id.TVMaxTemp)
-        sunRiseTV = findViewById(R.id.TVSunRise)
-        sundownTV = findViewById(R.id.TVSunDownTime)
-        windSpeedTV = findViewById(R.id.TVWindSpeed)
-        cloudyTV = findViewById(R.id.TVCloud)
-        humidityTV  = findViewById(R.id.TVHumidity)
-        rainyTV = findViewById(R.id.TVRainChance)
+        cityName = binding.ETCityName.text.toString()
 
-
-        searchIV.setOnClickListener{
-            cityName = cityNameET.text.toString()
+        binding.IVSearch.setOnClickListener{
+            cityName = binding.ETCityName.text.toString()
             if (cityName.isNotEmpty()) {
-                getWeather(cityName)
-                closeKeyBoard(searchIV)
+                GlobalScope.launch {
+                    weatherNetWorkCall(cityName)
+                    closeKeyBoard(binding.IVSearch)
+                }
             } else{
                 Toast.makeText(this, "Input a city name", Toast.LENGTH_SHORT).show()
-                closeKeyBoard(searchIV)
+                closeKeyBoard(binding.IVSearch)
             }
         }
-        cityNameET.setOnEditorActionListener { _, actionId, event ->
-            cityName = cityNameET.text.toString()
+        binding.ETCityName.setOnEditorActionListener { _, actionId, event ->
+            cityName = binding.ETCityName.text.toString()
             if (actionId == EditorInfo.IME_ACTION_DONE || event.keyCode == KEYCODE_ENTER && cityName.isNotEmpty()) {
-                getWeather(cityName)
-                closeKeyBoard(cityNameET)
+                GlobalScope.launch {
+                    weatherNetWorkCall(cityName)
+                    closeKeyBoard(binding.IVSearch)
+                }
                 true
             } else {
                 Toast.makeText(this, "Input a city name", Toast.LENGTH_SHORT).show()
-                closeKeyBoard(cityNameET)
+                closeKeyBoard(binding.ETCityName)
                 false
             }
         }
     }
 
-    @SuppressLint("SetTextI18n")
-    //@OptIn(DelicateCoroutinesApi::class)
-    private fun getWeather(cityName: String){
-            val getWeatherInfo = RetrofitInstance.getWeather(cityName).create(WeatherApi::class.java)
-            GlobalScope.launch {
-
-                val result =
-                    getWeatherInfo.getForecastWeather(com.example.rutgersweatherapp.BuildConfig.WEATHER_API_KEY, cityName, 7)
-                if (result.isSuccessful) {
-                    val data = result.body()!!
-                    withContext(Dispatchers.Main) {
-                        locationTV.text = data.location.name
-                        dateTV.text = data.location.localtime
-                        Picasso.get().load("http:" + data.current.condition.icon).into(currentWeatherIV)
-                        currentTempTV.text = data.current.temp_f.toString() + "° F"
-                        maxTempTV.text =
-                            data.forecast.forecastday[0].day.maxtemp_f.toString() + "° F"
-                        minTempTV.text =
-                            data.forecast.forecastday[0].day.mintemp_f.toString() + "° F"
-                        sunRiseTV.text = data.forecast.forecastday[0].astro.sunrise
-                        sundownTV.text = data.forecast.forecastday[0].astro.sunset
-                        windSpeedTV.text = data.current.wind_mph.toString() + "mph"
-                        cloudyTV.text = data.current.cloud.toString() + "%"
-                        rainyTV.text =
-                            data.forecast.forecastday[0].day.daily_chance_of_rain.toString() + "%"
-                        humidityTV.text =
-                            data.forecast.forecastday[0].day.avghumidity.toString() + "%"
-                    }
-                } else {
-                    return@launch
+    private suspend fun weatherNetWorkCall(cityName: String){
+        GlobalScope.launch {
+            val response = RetrofitInstance.getWeather().getForecastWeather(WEATHER_API_KEY, cityName, 7)
+            if (response.isSuccessful){
+                val data = response.body()!!
+                withContext(Dispatchers.Main){
+                    binding.TVLocation.text = data.location.name
+                    binding.TVDate.text = data.location.localtime
+                    Picasso.get().load("http:" + data.current.condition.icon)
+                        .into(binding.IVWeatherCondition)
+                    binding.TVTemperature.text = data.current.temp_f.toString()
+                    binding.TVMaxTemp.text = data.forecast.forecastday[0].day.maxtemp_f.toString() + "° F"
+                    binding.TVMinTemp.text = data.forecast.forecastday[0].day.mintemp_f.toString() + "° F"
+                    binding.TVSunRise.text = data.forecast.forecastday[0].astro.sunrise
+                    binding.TVSunDownTime.text = data.forecast.forecastday[0].astro.sunset
+                    binding.TVWindSpeed.text = data.current.wind_mph.toString() + "mph"
+                    binding.TVCloud.text = data.current.cloud.toString() + " %"
+                    binding.TVRainChance.text = data.forecast.forecastday[0].day.daily_chance_of_rain.toString() + " %"
+                    binding.TVHumidity.text = data.forecast.forecastday[0].day.avghumidity.toString() + " %"
                 }
+            } else {
+                return@launch
             }
+        }
     }
 
     private fun closeKeyBoard(view: View) {
--- app/src/main/java/com/example/rutgersweatherapp/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/rutgersweatherapp/api/RetrofitInstance.kt
@@ -2,17 +2,17 @@ package com.example.rutgersweatherapp.api
 
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
 
 object RetrofitInstance {
 
-    const val baseUrl ="https://api.weatherapi.com/v1/"
+    private const val baseUrl ="https://api.weatherapi.com/v1/"
 
-    fun getWeather(cityName: String): Retrofit {
-            return Retrofit
-                .Builder()
+    fun getWeather(): WeatherApi {
+            return Retrofit.Builder()
                 .baseUrl(baseUrl)
                 .addConverterFactory(MoshiConverterFactory.create())
                 .build()
-
+                .create(WeatherApi::class.java)
     }
 }
