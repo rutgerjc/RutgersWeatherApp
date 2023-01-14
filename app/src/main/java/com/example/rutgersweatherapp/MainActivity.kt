f8c22161c92136ac6f4f775f3a59ecb445045e51
rutgerjc <colonrutger@gmail.com>
2023-01-14 09:21:57 -0500

    ADDED Hilt Dependency and implemented Hilt

--- app/src/main/java/com/example/rutgersweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/rutgersweatherapp/MainActivity.kt
@@ -2,36 +2,62 @@ package com.example.rutgersweatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.KeyEvent.KEYCODE_ENTER
 import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
+import androidx.lifecycle.lifecycleScope
 import com.example.rutgersweatherapp.BuildConfig.WEATHER_API_KEY
-import com.example.rutgersweatherapp.api.RetrofitInstance
+import com.example.rutgersweatherapp.api.WeatherApi
 import com.example.rutgersweatherapp.databinding.ActivityMainBinding
 import com.squareup.picasso.Picasso
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.*
+import javax.inject.Inject
 
-
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     private lateinit var cityName: String
+
+    @Inject
+    lateinit var weatherApi: WeatherApi
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        cityName = binding.ETCityName.text.toString()
-
+             suspend fun weatherNetworkCall(cityName: String) {
+                 lifecycleScope.launchWhenStarted {
+                     val response = weatherApi.getForecastWeather(WEATHER_API_KEY, cityName, 7)
+                     Log.i("DATA", response.body()!!.toString())
+                     val data = response.body()!!
+                     withContext(Dispatchers.Main){
+                         binding.TVLocation.text = data.location.name
+                         binding.TVDate.text = data.location.localtime
+                         Picasso.get().load("http:" + data.current.condition.icon)
+                             .into(binding.IVWeatherCondition)
+                         binding.TVTemperature.text = data.current.temp_f.toString()
+                         binding.TVMaxTemp.text = data.forecast.forecastday[0].day.maxtemp_f.toString() + "° F"
+                         binding.TVMinTemp.text = data.forecast.forecastday[0].day.mintemp_f.toString() + "° F"
+                         binding.TVSunRise.text = data.forecast.forecastday[0].astro.sunrise
+                         binding.TVSunDownTime.text = data.forecast.forecastday[0].astro.sunset
+                         binding.TVWindSpeed.text = data.current.wind_mph.toString() + "mph"
+                         binding.TVCloud.text = data.current.cloud.toString() + " %"
+                         binding.TVRainChance.text = data.forecast.forecastday[0].day.daily_chance_of_rain.toString() + " %"
+                         binding.TVHumidity.text = data.forecast.forecastday[0].day.avghumidity.toString() + " %"
+                     }
+                 }
+            }
         binding.IVSearch.setOnClickListener{
             cityName = binding.ETCityName.text.toString()
             if (cityName.isNotEmpty()) {
                 GlobalScope.launch {
-                    weatherNetWorkCall(cityName)
+                    weatherNetworkCall(cityName)
                     closeKeyBoard(binding.IVSearch)
                 }
             } else{
@@ -43,7 +69,7 @@ class MainActivity : AppCompatActivity() {
             cityName = binding.ETCityName.text.toString()
             if (actionId == EditorInfo.IME_ACTION_DONE || event.keyCode == KEYCODE_ENTER && cityName.isNotEmpty()) {
                 GlobalScope.launch {
-                    weatherNetWorkCall(cityName)
+                    weatherNetworkCall(cityName)
                     closeKeyBoard(binding.IVSearch)
                 }
                 true
@@ -54,33 +80,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
-    private suspend fun weatherNetWorkCall(cityName: String){
-        GlobalScope.launch {
-            val response = RetrofitInstance.getWeather().getForecastWeather(WEATHER_API_KEY, cityName, 7)
-            if (response.isSuccessful){
-                val data = response.body()!!
-                withContext(Dispatchers.Main){
-                    binding.TVLocation.text = data.location.name
-                    binding.TVDate.text = data.location.localtime
-                    Picasso.get().load("http:" + data.current.condition.icon)
-                        .into(binding.IVWeatherCondition)
-                    binding.TVTemperature.text = data.current.temp_f.toString()
-                    binding.TVMaxTemp.text = data.forecast.forecastday[0].day.maxtemp_f.toString() + "° F"
-                    binding.TVMinTemp.text = data.forecast.forecastday[0].day.mintemp_f.toString() + "° F"
-                    binding.TVSunRise.text = data.forecast.forecastday[0].astro.sunrise
-                    binding.TVSunDownTime.text = data.forecast.forecastday[0].astro.sunset
-                    binding.TVWindSpeed.text = data.current.wind_mph.toString() + "mph"
-                    binding.TVCloud.text = data.current.cloud.toString() + " %"
-                    binding.TVRainChance.text = data.forecast.forecastday[0].day.daily_chance_of_rain.toString() + " %"
-                    binding.TVHumidity.text = data.forecast.forecastday[0].day.avghumidity.toString() + " %"
-                }
-            } else {
-                return@launch
-            }
-        }
-    }
-
     private fun closeKeyBoard(view: View) {
         val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
--- app/src/main/java/com/example/rutgersweatherapp/api/RetrofitInstance.kt
+++ app/src/main/java/com/example/rutgersweatherapp/api/RetrofitInstance.kt
@@ -1,18 +1,36 @@
 package com.example.rutgersweatherapp.api
 
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import org.intellij.lang.annotations.PrintFormat
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.create
+import javax.inject.Singleton
 
+
+@Module
+@InstallIn(SingletonComponent::class)
 object RetrofitInstance {
 
-    private const val baseUrl ="https://api.weatherapi.com/v1/"
+    //private const val baseUrl ="https://api.weatherapi.com/v1/"
 
-    fun getWeather(): WeatherApi {
+
+    @Provides
+    @Singleton
+    fun getWeather(): Retrofit {
             return Retrofit.Builder()
-                .baseUrl(baseUrl)
+                .baseUrl("https://api.weatherapi.com/v1/")
                 .addConverterFactory(MoshiConverterFactory.create())
                 .build()
-                .create(WeatherApi::class.java)
+                //.create(WeatherApi::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun weathersService(retrofit: Retrofit): WeatherApi{
+        return retrofit.create(WeatherApi::class.java)
     }
 }
