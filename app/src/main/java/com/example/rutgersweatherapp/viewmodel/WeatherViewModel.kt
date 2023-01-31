5da190a3befaed8f837c94b3690aa6b402fdf2b2
rutgerjc <colonrutger@gmail.com>
2023-01-31 18:25:21 -0500

    added ViewModel and completed app MVVM architecture design

--- app/src/main/java/com/example/rutgersweatherapp/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/com/example/rutgersweatherapp/viewmodel/WeatherViewModel.kt
@@ -1,19 +1,13 @@
 package com.example.rutgersweatherapp.viewmodel
 
-import android.util.Log
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewModelScope
 import com.example.rutgersweatherapp.BuildConfig
 import com.example.rutgersweatherapp.api.WeatherApi
 import com.example.rutgersweatherapp.data.CurrentWeatherResponse
 import dagger.hilt.android.lifecycle.HiltViewModel
-import dagger.hilt.android.scopes.ViewModelScoped
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 @HiltViewModel
