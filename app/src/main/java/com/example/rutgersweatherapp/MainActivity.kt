0064cb508d066a6f78ade9e693fdd9ef9d089f64
rutgerjc <colonrutger@gmail.com>
2023-02-13 10:38:46 -0500

    fixed suspend issue that was causing crash

--- app/src/main/java/com/example/rutgersweatherapp/MainActivity.kt
+++ app/src/main/java/com/example/rutgersweatherapp/MainActivity.kt
@@ -13,6 +13,7 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.lifecycleScope
 import com.example.rutgersweatherapp.BuildConfig.WEATHER_API_KEY
 import com.example.rutgersweatherapp.api.WeatherApi
+import com.example.rutgersweatherapp.data.CurrentWeatherResponse
 import com.example.rutgersweatherapp.databinding.ActivityMainBinding
 import com.example.rutgersweatherapp.viewmodel.WeatherViewModel
 import com.squareup.picasso.Picasso
@@ -36,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         binding.IVSearch.setOnClickListener{
             cityName = binding.ETCityName.text.toString()
             if (cityName.isNotEmpty()) {
-                GlobalScope.launch {
+                lifecycleScope.launch{
                     viewModel.weatherCall(cityName, 7)
                     closeKeyBoard(binding.IVSearch)
                 }
@@ -48,7 +49,7 @@ class MainActivity : AppCompatActivity() {
         binding.ETCityName.setOnEditorActionListener { _, actionId, event ->
             cityName = binding.ETCityName.text.toString()
             if (actionId == EditorInfo.IME_ACTION_DONE || event.keyCode == KEYCODE_ENTER && cityName.isNotEmpty()) {
-                GlobalScope.launch {
+                lifecycleScope.launch{
                     viewModel.weatherCall(cityName, 7)
                     closeKeyBoard(binding.IVSearch)
                 }
@@ -77,6 +78,7 @@ class MainActivity : AppCompatActivity() {
             binding.TVHumidity.text = data.forecast.forecastday[0].day.avghumidity.toString() + " %"
         }
     }
+
     private fun closeKeyBoard(view: View) {
         val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
